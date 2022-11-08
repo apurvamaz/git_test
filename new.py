e6266f91e329c6dfcbf83f6c8deb98f583ab63af
apurvamaz <apurv.n@amazatic.com>
2022-11-08 15:41:06 +0530

    new file added we.py and upated new.py

--- new.py
+++ new.py
@@ -1,5 +1,17 @@
 from time import sleep
 
+print("*"*50)
+print("This is the git test file")
+print("*"*50)
+
 for i in range(1,11):
     print(i, "-- Hello")
     sleep(0.2)
+
+print("*"*50)
+for j in range(1,5):
+    print(j)
+
+
+
+
